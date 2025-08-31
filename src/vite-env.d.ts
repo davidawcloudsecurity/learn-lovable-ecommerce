fad5a991e2510471bd899d650e25b6b951febbca
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-08-31 12:59:02 +0800

    Update vite-env.d.ts - revert

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,13 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_S3_BUCKET_URL?: string;
-  readonly VITE_SUPABASE_URL: string;
-  readonly VITE_SUPABASE_PUBLISHABLE_KEY: string;
-  readonly VITE_SUPABASE_PROJECT_ID: string;
-  // more env variables...
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
