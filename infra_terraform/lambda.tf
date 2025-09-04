cfbde32daaa189cf10a30d7714952563aca54fee
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-09-04 15:43:16 +0800

    Update lambda.tf - fixed local

--- infra_terraform/lambda.tf
+++ infra_terraform/lambda.tf
@@ -34,7 +34,7 @@ data "archive_file" "lambda_zip" {
 resource "aws_lambda_function" "api" {
   filename         = data.archive_file.lambda_zip.output_path
   function_name    = "ecommerce-api"
-  role            = aws_iam_role.lambda_role.arn
+  role            = local.lambda_role.arn
   handler         = "lambda.handler"
   runtime         = "nodejs16.x"
   timeout         = 30
