4477b9efaf9cf80c455da71477229b6f802d310d
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-09-07 09:38:21 +0800

    Update lambda.tf - added npm install @vendia/serverless-express

--- infra_terraform/lambda.tf
+++ infra_terraform/lambda.tf
@@ -10,6 +10,7 @@ resource "null_resource" "lambda_zip" {
   provisioner "local-exec" {
     command = <<-EOT
       cd ../lambda
+      npm install @vendia/serverless-express
       zip -r lambda-api.zip . -x "*.zip" "node_modules/*"
     EOT
   }
