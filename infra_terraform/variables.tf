032361e89993ce19cbb0d96d5cf5eefeca35b10f
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2026-04-09 17:04:13 +0800

    Update variables.tf

--- infra_terraform/variables.tf
+++ infra_terraform/variables.tf
@@ -7,7 +7,7 @@ variable "backup_vault_lock_changeable_for_days" {
 variable "backup_vault_lock_min_retention_days" {
   description = "Minimum retention days for vault lock"
   type        = number
-  default     = 15
+  default     = 1
 }
 
 variable "backup_vault_lock_max_retention_days" {
