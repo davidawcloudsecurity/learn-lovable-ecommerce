0595abc58a568871702f797ac6483b540a08cd64
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2026-04-09 16:03:06 +0800

    Update and rename start.sh to variables.tf

--- infra_terraform/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo yum install -y yum-utils shadow-utils; sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo; sudo yum -y install terraform
-echo 'alias tf="terraform"; alias tfa="terraform apply --auto-approve"; alias tfd="terraform destroy --auto-approve"; alias tfm="terraform init; terraform fmt; terraform validate; terraform plan"' >> ~/.bashrc
-source ~/.bashrc
-terraform init
---
+++ infra_terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "backup_vault_lock_changeable_for_days" {
+  description = "Number of days the vault lock is changeable"
+  type        = number
+  default     = 30
+}
+
+variable "backup_vault_lock_min_retention_days" {
+  description = "Minimum retention days for vault lock"
+  type        = number
+  default     = 15
+}
+
+variable "backup_vault_lock_max_retention_days" {
+  description = "Maximum retention days for vault lock"
+  type        = number
+  default     = 2555
+}
